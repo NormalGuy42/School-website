cd94bed43903208705e92a831d9bb91d4576ff0f
Madiou <diabatluffy@gmail.com>
2023-03-01 18:06:27 +0000

    Initial commit

---
+++ iuheg/script.js
@@ -0,0 +1,13 @@
+const navBtn = document.querySelector('.burger');
+navBtn.addEventListener('click',()=>{
+    const menu = document.querySelector('.menu')
+    menu.classList.toggle('show')
+})
+document.addEventListener('click',(e)=>{
+    const menu = document.querySelector('.menu')
+    if(!e.target.closest('.burger') && menu.classList.contains('show')){
+        if(!e.target.closest('.menu')){
+            menu.classList.remove('show');
+        }
+    }
+})
