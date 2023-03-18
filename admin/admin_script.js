d745d5c99471477c9369571602f8d415a28fe148
Madiou <diabatluffy@gmail.com>
2023-03-18 15:33:46 +0000

    Prettying up the homepage

--- admin/admin_script.js
+++ admin/admin_script.js
@@ -1,3 +1,12 @@
+//Menu 
+var burger = document.querySelector('.burger')
+burger.addEventListener('click',()=>{
+    var sideBar = document.querySelector('.sidebar');
+    sideBar.classList.toggle('showSidebar');
+})
+
+
+
 var collapsables = document.querySelectorAll('.collapsable');
 collapsables.forEach(collapsable=>{
     collapsable.addEventListener('click',()=>{
@@ -5,6 +14,14 @@ collapsables.forEach(collapsable=>{
         var svg = collapsable.querySelector('svg');
         svg.classList.toggle('rotate_svg');
         section.classList.toggle('showSection');
-        console.log('clicked');
     })
+})
+
+document.addEventListener('click',(e)=>{
+    if(!e.target.closest('.burger') ){
+        var sideBar = document.querySelector('.sidebar');
+        if(!e.target.closest('.sidebar')&& sideBar.classList.contains('showSidebar')){
+            sideBar.classList.remove('showSidebar')
+        }
+    }
 })
