155b7f42f4a57de4a3cb72e6946d29d103df0930
Madiou <diabatluffy@gmail.com>
2023-03-19 18:14:36 +0000

    New commit

--- Lycée-primaire/script.js
+++ Lycée-primaire/script.js
@@ -3,3 +3,11 @@ const menu = document.querySelector('.menu')
 menuBtn.addEventListener('click',()=>{
     menu.classList.toggle('show');
 })
+
+document.addEventListener('click',(e)=>{
+    if(!e.target.closest('.burger') && menu.classList.contains('show')){
+        if(!e.target.closest('.menu')){
+            menu.classList.remove('show');
+        }
+    }
+})
